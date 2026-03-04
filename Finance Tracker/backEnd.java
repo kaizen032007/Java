a08edd6493973758505db0c9d47719d6c1203ad4
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-03-04 22:48:41 +0800

    Update backEnd.java

--- Finance Tracker/backEnd.java
+++ Finance Tracker/backEnd.java
@@ -4,13 +4,32 @@ public class backEnd {
 
     // this method allows the print to be centered
     public static void printCentered(String text) {
-    int terminalWidth = 65; 
-    int padding = (terminalWidth - text.length()) / 2;
-    String centered = " ".repeat(padding) + text;
-    System.out.println(centered);
+        int terminalWidth = 65;
+        int padding = (terminalWidth - text.length()) / 2;
+        String centered = " ".repeat(padding) + text;
+        System.out.println(centered);
     }
 
-    public static void  validInput(frontEnd frontEnd, Scanner scanner) {
-        
+    public void financeTrackerMainWindowOptions(Scanner scanner, int options) {
+        boolean validInput = false;
+
+        while (!validInput) {
+            if (options == 1) {
+                this.addMoney(scanner);
+            }
+        }
+    }
+
+    public void addMoney(Scanner scanner) {
+        System.out.print("Do you want to add Money? ");
+        String option = scanner.nextLine();
+
+        if (option.equalsIgnoreCase("y") && option.equalsIgnoreCase("n")) {
+
+        }
+
+        System.out.println("=".repeat(70));
+        printCentered("Proceeding to add money please wait");
+        System.out.println("=".repeat(70));
     }
 }
