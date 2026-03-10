d97056d3bc97289b0eb1cddadc51938cea076c5b
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-03-10 21:43:40 +0800

    Update backEnd.java

--- Finance Tracker/backEnd.java
+++ Finance Tracker/backEnd.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class backEnd {
 
+    private String name;
+    private int age;
+
     // this method allows the print to be centered
     public static void printCentered(String text) {
         int terminalWidth = 65;
@@ -11,10 +14,26 @@ public class backEnd {
         System.out.println(centered);
     }
 
+    public void loadingBar() {
+        // Loading bar over 10 seconds
+        System.out.print("Loading: [");
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000); // 1 second per step
+            } catch (InterruptedException time) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.print("="); // Print one bar per second
+        }
+    }
+
     // this method is the backEnd options for the finance tracker main window
     public void financeTrackerMainWindowOptions(Scanner scanner, int options, FrontEnd frontEnd,
-            InputMismatchException e) {
+            InputMismatchException e, String name) {
         boolean validInput = false;
+
+        this.name = name;
+        this.age = age;
 
         while (!validInput) {
             try {
@@ -49,13 +68,44 @@ public class backEnd {
 
                 if (option.equalsIgnoreCase("n")) {
                     System.out.println("Going back!");
-                    this.financeTrackerMainWindowOptions(scanner, 0, frontEnd, e);
+                    this.financeTrackerMainWindowOptions(scanner, 0, frontEnd, e, name);
                     return;
                 }
 
                 validInput = true;
-                System.out.println("Please wait....");
 
+            } catch (InputMismatchException inputError) {
+                System.out.println("Error! " + inputError.getMessage());
+            }
+        }
+
+        System.out.println("Please wait....");
+        loadingBar();
+        System.out.println("] Done!");
+        System.out.println("Going in!");
+
+        System.out.println("=".repeat(70));
+        printCentered("Welcome to your Deposit Dashboard! " + name);
+        printCentered("Here you can add your money to an existing balance");
+        System.out.println("=".repeat(70));
+
+        System.out.print("Do you need help? (y/n): ");
+        String help = scanner.nextLine();
+
+        validInput = false;
+        while (!validInput) {
+            try {
+                if (!help.equalsIgnoreCase("y") && !help.equalsIgnoreCase("n")) {
+                    throw new InputMismatchException("Your option must be Y or N only");
+                }
+
+                if (help.equalsIgnoreCase("y")) {
+                    validInput = true;
+                    System.out.println("Great let me help you!");
+                    System.out.println(
+                            "This section is where you can deposit or add your money to an existing balance and to do so follow these steps.");
+                    System.out.println("1. Enter the amount of money you wish to add");
+                }
             } catch (InputMismatchException inputError) {
                 System.out.println("Error! " + inputError.getMessage());
             }
