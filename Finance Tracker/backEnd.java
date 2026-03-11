cd6d298cecda399c84e8c4a872c2b2db9bacede1
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-03-11 18:40:26 +0800

    Update backEnd.java

--- Finance Tracker/backEnd.java
+++ Finance Tracker/backEnd.java
@@ -5,6 +5,7 @@ public class backEnd {
 
     private String name;
     private int age;
+    private double balance = 0.0;
 
     // this method allows the print to be centered
     public static void printCentered(String text) {
@@ -80,7 +81,7 @@ public class backEnd {
         }
 
         System.out.println("Please wait....");
-        loadingBar();
+        // loadingBar();
         System.out.println("] Done!");
         System.out.println("Going in!");
 
@@ -105,10 +106,66 @@ public class backEnd {
                     System.out.println(
                             "This section is where you can deposit or add your money to an existing balance and to do so follow these steps.");
                     System.out.println("1. Enter the amount of money you wish to add");
+
+                    System.out.println("");
+                }
+
+                if (help.equalsIgnoreCase("n")) {
+                    validInput = true;
+                    System.out.println("Good! Wait to proceed");
                 }
             } catch (InputMismatchException inputError) {
                 System.out.println("Error! " + inputError.getMessage());
             }
         }
+
+        validInput = false;
+        while (!validInput) {
+            try {
+                System.out.println("");
+                System.out.println("=".repeat(70));
+                System.out.println("Current Balance: " + balance);
+                System.out.print("Enter the money you wish to add: ");
+                int addMoney = scanner.nextInt();
+                scanner.nextLine();
+
+                if (addMoney <= 0) {
+                    throw new InputMismatchException("You must put more than 0");
+                }
+
+                balance += addMoney;
+                System.out.println("Success!");
+                System.out.println("New Balance: " + balance);
+                validInput = true;
+            } catch (InputMismatchException moneyInputError) {
+                System.out.println("Error!  " + moneyInputError.getMessage());
+            }
+        }
+
+        validInput = false;
+        while (!validInput) {
+            try {
+                System.out.print("Do you want to create another transaction? (y/n): ");
+                String option = scanner.nextLine();
+
+                if (!option.equalsIgnoreCase("y") && !option.equalsIgnoreCase("n")) {
+                    throw new InputMismatchException("Your input must be Y or N only");
+                }
+
+                if (option.equalsIgnoreCase("y")) {
+                    System.out.println("Okay going back!");
+                    this.addMoney(scanner, frontEnd, e);
+                }
+
+                if (option.equalsIgnoreCase("n")) {
+                    System.out.println("Great!");
+                    this.financeTrackerMainWindowOptions(scanner, age, frontEnd, e, option);
+                    return;
+                }
+            } catch (InputMismatchException errorInput) {
+                System.out.println("Error!" + errorInput.getMessage());
+            }
+        }
+
     }
 }
