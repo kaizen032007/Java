c36772d5d700d3e89f5709374873c9940b6cf669
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-03-30 21:34:52 +0800

    Create App.java JavaFX Application that loads LoginView.fxml

---
+++ EducPhil/educphil-frontend/src/main/java/com/educphil/frontend/App.java
@@ -0,0 +1,21 @@
+package com.educphil.frontend;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
