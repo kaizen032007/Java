00d65122ced8dfde18b68182fe21b5b271e97eae
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-04-27 12:40:48 +0800

    Update IncomeTransaction.java

--- Sora /src/main/java/com/sora/IncomeTransaction.java
+++ Sora /src/main/java/com/sora/IncomeTransaction.java
@@ -1,14 +1,1 @@
-package com.sora;
 
-public class IncomeTransaction extends Transaction {
-
-    IncomeTransaction(String transactionId, String userId, String type, double amount,
-            String category, String description, String date) {
-        super(transactionId, userId, type, amount, category, description, date);
-    }
-
-    @Override
-    public String getTransactionCategory() {
-        return "This is an income transaction";
-    }
-}
