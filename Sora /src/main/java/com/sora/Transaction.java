7b1c2144fb54864d472ea6df95d69ac16ae21295
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-04-27 23:53:43 +0800

    Update Transaction.java

--- Sora /src/main/java/com/sora/Transaction.java
+++ Sora /src/main/java/com/sora/Transaction.java
@@ -1,1 +1,20 @@
+package com.sora.model;
 
+public class Transaction {
+    private String transactionId;
+    private String description;
+    private String category;
+    private double amount;
+    private String date;
+    private String type;
+
+    Transaction(String transactionId, String description, String category, double amount, String date, String type) {
+        this.transactionId = transactionId;
+        this.description = description;
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+        this.type = type;
+    }
+
+}
