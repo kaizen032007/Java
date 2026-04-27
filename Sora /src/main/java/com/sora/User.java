49d4ff40e7caffd5213cf86bdc765e5a71ce5004
CeeJ <orcinoceej57@gmail.com>
2026-04-27 21:46:56 +0800

    Update User.java

--- Sora /src/main/java/com/sora/User.java
+++ Sora /src/main/java/com/sora/User.java
@@ -1,1 +1,49 @@
+package com.sora;
 
+public class User {
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(String userId, String firstName, String lastName, String email, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    public boolean validatePassword(String input) {
+        return this.password.equals(input);
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + userId +
+               ", Name: " + firstName + " " + lastName +
+               ", Email: " + email;
+    }
+}
