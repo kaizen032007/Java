34173e8f58952a9c5cf9e5453dddee2cdb375910
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-04-27 12:41:06 +0800

    Update ExpenseTransaction.java

--- Sora /src/main/java/com/sora/ExpenseTransaction.java
+++ Sora /src/main/java/com/sora/ExpenseTransaction.java
@@ -1,14 +1,1 @@
-package com.sora;
 
-public class ExpenseTransaction extends Transaction {
-
-    ExpenseTransaction(String transactionId, String userId, String type, double amount,
-            String category, String description, String date) {
-        super(transactionId, userId, type, amount, category, description, date);
-    }
-
-    @Override
-    public String getTransactionCategory() {
-        return "This is an expense transaction";
-    }
-}
