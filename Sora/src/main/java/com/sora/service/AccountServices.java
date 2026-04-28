dbc68f0c48a51edd66bee64836d283a3e8f97254
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-04-28 20:20:05 +0800

    Create AccountServices.java

---
+++ Sora/src/main/java/com/sora/service/AccountServices.java
@@ -0,0 +1,16 @@
+package com.sora.service;
+
+import com.sora.model.Account;
+import java.util.List;
+import java.util.ArrayList;
+
+public class AccountServices {
+    private List<Account> accounts;
+
+
+    AccountServices() {
+        try {
+            
+        }
+    }
+}
