347bad7f3520af49f6fe074ddb4b970a00a8d80f
Jae Mark Almeria <jaemarkalmeria08@gmail.com>
2026-05-20 00:32:50 +0800

    Delete Sora/src/main/java/com/sora directory

--- Sora/src/main/java/com/sora/model/Budget.java
+++
@@ -1,5 +0,0 @@
-package com.sora.model;
-
-public class Budget {
-    
-}
--- Sora/src/main/java/com/sora/ui/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.sora.ui;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // This bypasses the JavaFX runtime check on startup
-        MainApp.main(args);
-    }
-}
